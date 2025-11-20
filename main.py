2439721ee4b345fbc715210c01c6b0f9bf5668aa
het-tale <ettalebyhasnaa99@gmail.com>
2025-11-20 17:14:26 +0100

    feat: initiate connection to DB at application startup and close it at shutdown

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from typing import Annotated
+from asyncpg import Connection
+from fastapi import Depends, FastAPI
+
+from src.database.connection_db import connection
+
+connection_dep = Annotated[Connection, Depends(connection.get_connection)]
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await connection.init_connection()
+    yield
+    await connection.close_connection()
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
 def say_hello():
-    return {"message": "hello Matcha"}
+    return {"message": "hello Matha"}
--- src/database/connection_db.py
+++ src/database/connection_db.py
@@ -1,10 +1,37 @@
-async def init_postgres_conn():
-    pass
+import asyncpg
+from loguru import logger
+from typing import Optional
+from src.core.config import settings
 
 
-async def get_postgres_conn():
-    pass
+class DatabaseConnection:
+    def __init__(self):
+        self.connection_pool: Optional[asyncpg.Pool] = None
+
+    async def init_connection(self):
+        logger.info("initiation of DB")
+        try:
+            self.connection_pool = await asyncpg.create_pool(
+                dsn=settings.DATABASE_URL,
+                min_size=1,
+                max_size=10
+            )
+        except Exception as e:
+            logger.error("Error while creating connection pool", e)
+            raise
+
+    async def get_connection(self):
+        if self.connection_pool is None:
+            logger.error("The connection pool is NULL")
+            raise
+        return self.connection_pool
+
+    async def close_connection(self):
+        logger.info("Closing Database connection")
+        try:
+            await self.connection_pool.close()
+        except Exception as e:
+            logger.error("Error occurred during close", e)
 
 
-async def close_postgres_conn():
-    pass
+connection = DatabaseConnection()
